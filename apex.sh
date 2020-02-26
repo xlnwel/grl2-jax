8e93e9d2ac326ff3d028bf5aa3c31c1e336959b8
S.C <122134545@qq.com>
2020-02-26 08:13:26 +0800

    Baseline

--- apex.sh
+++ apex.sh
@@ -1,8 +1,10 @@
 source activate drl
 
-# python run/train.py -a apex-sac -e Walker2d-v2
-python run/train.py -a apex-sac -e HalfCheetah-v2
-python run/train.py -a apex-sac -e Swimmer-v2 -ms 2e7
+python run/train.py -a apex-sac -e HalfCheetah-v2 -p 3
+python run/train.py -a apex-sac -e Walker2d-v2 -p 2
+python run/train.py -a apex-sac -e Walker2d-v2 -p 3
+python run/train.py -a apex-sac -e Hopper-v2 -p 3
+# python run/train.py -a apex-sac -e Swimmer-v2 -ms 2e7
 # python run/train.py -a apex-sac -e Reacher-v2
 # python run/train.py -a apex-sac -e Humanoid-v2 -ms 3e7 -p n_envs=2
 # python run/train.py -a apex-sac -e Ant-v2 -ms 2e7
--- asap2.sh
+++
@@ -1,9 +0,0 @@
-source activate drl
-
-python run/train.py -a asap2-sac -e Walker2d-v2
-python run/train.py -a asap2-sac -e HalfCheetah-v2
-# python run/train.py -a asap2-sac -e Swimmer-v2 -ms 2e7
-# python run/train.py -a asap2-sac -e Reacher-v2
-# python run/train.py -a asap2-sac -e Humanoid-v2
-# python run/train.py -a asap2-sac -e Hopper-v2
-# python run/train.py -a asap2-sac -e Ant-v2
