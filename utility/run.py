d7c68a070ad2337264e7c861a72e96f7795c5abe
S.C <122134545@qq.com>
2020-05-20 22:31:05 +0800

    Use info to optimize envvec

--- utility/run.py
+++ utility/run.py
@@ -1,7 +1,9 @@
 from collections import deque
 import numpy as np
+
 from utility.display import pwc
 from utility.timer import TBTimer
+from env.gym_env import Env
 
 
 class Runner:
@@ -9,6 +11,7 @@ class Runner:
         self.env = env
         self.agent = agent
         self.obs = env.reset()
+        self.reset = np.ones(env.n_envs)
         self.step = step
 
         self._frame_skip = getattr(env, 'frame_skip', 1)
@@ -18,6 +21,12 @@ class Runner:
 
     def run(self, *, action_selector=None, step_fn=None, nsteps=None):
         """ run `nstep` agent steps, auto reset if an episodes is done """
+        if isinstance(self.env, Env):
+            return self._run_env(action_selector, step_fn, nsteps)
+        else:
+            return self._run_envvec(action_selector, step_fn, nsteps)
+
+    def _run_env(self, action_selector, step_fn, nsteps):
         env = self.env
         obs = self.obs
         action_selector = action_selector or self.agent
@@ -26,10 +35,10 @@ class Runner:
 
         for t in range(nsteps):
             action = action_selector(obs, 
-                        reset=env.already_done(), deterministic=False)
+                        reset=self.reset, deterministic=False)
             if isinstance(action, tuple):
                 action, terms = action
-            next_obs, reward, done, _ = env.step(action)
+            next_obs, reward, done, info = env.step(action)
 
             self.step += self._frames_per_step
             if step_fn:
@@ -40,19 +49,49 @@ class Runner:
                 step_fn(env, self.step, **kwargs)
             obs = next_obs
             # logging and reset 
-            if np.any(env.already_done()):
-                if env.n_envs == 1:
-                    if env.game_over():
-                        self.agent.store(
-                            score=env.score(), epslen=env.epslen())
-                    obs = env.reset()
-                else:
-                    idxes = [i for i, d in enumerate(env.game_over()) if d]
-                    if idxes:
-                        self.agent.store(
-                            score=env.score(idxes), epslen=env.epslen(idxes))
-                    for i, d in enumerate(env.already_done()):
-                        if d:   obs[i] = env.envs[i].reset()
+            if info.get('already_done'):
+                if info.get('game_over'):
+                    self.agent.store(score=info['score'], epslen=info['epslen'])
+                obs = env.reset()
+            self.reset = env.already_done()
+        self.obs = obs
+
+        return self.step
+
+    def _run_envvec(self, action_selector, step_fn, nsteps):
+        env = self.env
+        obs = self.obs
+        action_selector = action_selector or self.agent
+        nsteps = nsteps or self._default_nsteps
+        terms = {}
+
+        for t in range(nsteps):
+            action = action_selector(obs, 
+                        reset=self.reset, deterministic=False)
+            if isinstance(action, tuple):
+                action, terms = action
+            next_obs, reward, done, info = env.step(action)
+
+            self.step += self._frames_per_step
+            if step_fn:
+                kwargs = dict(obs=obs, action=action, reward=reward,
+                    discount=1-done, nth_obs=next_obs)
+                # allow terms to overwrite the values in kwargs
+                kwargs.update(terms)
+                step_fn(env, self.step, **kwargs)
+            obs = next_obs
+            # logging and reset 
+            done_env_ids = [i for i, ii in enumerate(info) if ii.get('already_done')]
+            if done_env_ids:
+                score = [i['score'] for i in info if 'score' in i]
+                if score:
+                    epslen = [i['epslen'] for i in info if 'epslen' in i]
+                    self.agent.store(score=score, epslen=epslen)
+                
+                new_obs = env.reset(done_env_ids)
+                for i, o in zip(done_env_ids, new_obs):
+                    obs[i] = o
+            self.reset = np.array([i.get('already_done', False) for i in info])
         self.obs = obs
 
         return self.step
@@ -87,9 +126,10 @@ class Runner:
                     else:
                         obs = env.reset()
                 else:
-                    for i, d in enumerate(env.already_done()):
-                        if d and not env.envs[i].game_over():   
-                            obs[i] = env.envs[i].reset()
+                    idxes = [i for i, d in enumerate(env.game_over()) if d]
+                    new_obs = env.reset(idxes)
+                    for i, o in zip(idxes, new_obs):
+                        obs[i] = o
         self.agent.store(score=env.score(), epslen=env.epslen())
 
         return self.step
@@ -114,17 +154,25 @@ def evaluate(env, agent, n=1, record=False, size=None, video_len=1000):
             action = agent(obs, deterministic=True)
             if isinstance(action, tuple):
                 action = action[0]
-            obs, reward, done, _ = env.step(action)
+            obs, reward, done, info = env.step(action)
             
             if np.all(env.game_over()):
                 break
-            if np.any(env.already_done()):
-                if env.n_envs == 1:
+            if env.n_envs == 1:
+                if info.get('already_done'):
                     obs = env.reset()
-                else:
-                    for i, e in enumerate(env.envs):
-                        if e.already_done() and not e.game_over():
-                            obs[i] = e.reset()
+            else:
+                done_env_ids = [i for i, ii in enumerate(info) if ii.get('already_done')]
+                if done_env_ids:
+                    score = [i['score'] for i in info if 'score' in i]
+                    if score:
+                        epslen = [i['epslen'] for i in info if 'epslen' in i]
+                        agent.store(score=score, epslen=epslen)
+                    
+                    reset_env_ids = [i for i in done_env_ids if not info[i]['game_over']]
+                    new_obs = env.reset(reset_env_ids)
+                    for i, o in zip(reset_env_ids, new_obs):
+                        obs[i] = o
         scores.append(env.score())
         epslens.append(env.epslen())
     
