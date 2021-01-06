Fix RMS normalization

algo/ppo/buffer.py
@@ -101,7 +101,8 @@ class Buffer:
             self._memory[key][mb_idxes] = value
         elif field == 'all':
             assert self._memory[key].shape == value.shape, (self._memory[key].shape, value.shape)
-            self._memory[key] == value
+            self._memory[key] = value
+            print(key, np.max(self._memory[key]), np.max(value))
         else:
             raise ValueError(f'Unknown field: {field}. Valid fields: ("all", "mb")')
 

core/base.py
@@ -144,10 +144,11 @@ class RMSBaseAgent(BaseAgent):
                 Yeah, you may not find the pseudocode. That's why I quote:-)
                 """
                 assert discount is not None, \
-                    f"Normalizing rewards with reversed return requires environment's discount(i.e., 1-done) signals"
-                assert reward.ndim == discount.ndim == len(self._reward_rms.axis), (reward.shape, discount.shape, self._reward_rms.axis)
-                ret = backward_discounted_sum(self._reverse_return, reward, discount, self._gamma)
-                self._reverse_return = ret[:, -1] if reward.ndim == 2 else ret
+                    f"Normalizing rewards with reversed return requires environment's reset signals"
+                assert reward.ndim == discount.ndim == len(self._reward_rms.axis), \
+                    (reward.shape, discount.shape, self._reward_rms.axis)
+                self._reverse_return, ret = backward_discounted_sum(
+                    self._reverse_return, reward, discount, self._gamma)
                 self._reward_rms.update(ret)
             else:
                 self._reward_rms.update(reward)
@@ -176,14 +177,15 @@ def backward_discounted_sum(prev_ret, reward, discount, gamma,):
     assert reward.ndim == discount.ndim, (reward.shape, discount.shape)
     if reward.ndim == 1:
         prev_ret = reward + gamma * prev_ret
+        ret = prev_ret.copy()
         prev_ret *= discount
-        return prev_ret
+        return prev_ret, ret
     elif reward.ndim == 2:
         _nenv, nstep = reward.shape
         ret = np.zeros_like(reward)
         for t in range(nstep):
-            prev_ret = ret[:, t] = reward[:, t] + gamma * prev_ret
-            prev_ret *= discount[:, t]
-        return ret
+            ret[:, t] = prev_ret = reward[:, t] + gamma * prev_ret
+            prev_ret *= discount
+        return prev_ret, ret
     else:
         raise ValueError(f'Unknown reward shape: {reward.shape}')

nn/cnns/impala.py
@@ -58,11 +58,14 @@ class IMPALACNN(Module):
                 ]
             
             out_act_cls = get_activation(out_activation, return_cls=True)
-            self._layers.append(out_act_cls(name=prefix+out_activation))
+            self._layers += [
+                out_act_cls(name=prefix+out_activation)
+            ]
             self._flat = layers.Flatten(name=prefix+'flatten')
-            
             self.out_size = out_size
             if self.out_size:
+                if out_activation is None:
+                    self._layers.append[layers.ReLU()]
                 self._dense = layers.Dense(self.out_size, activation=out_act_cls(), name=prefix+'out')
         
         self._training_cls += [subsample_cls, block_cls]
@@ -77,6 +80,8 @@ class IMPALACNN(Module):
         x = self._flat(x)
         if self._time_distributed:
             x = tf.reshape(x, [-1, t, *x.shape[1:]])
+        
         if self.out_size:
             x = self._dense(x)
+
         return x

nn/cnns/procgen.py
@@ -79,37 +79,34 @@ class ProcgenCNN(Module):
                         sa_cls(name=f'{prefix}{sa}_{i}', **sa_kwargs)
                     ]
             out_act_cls = get_activation(out_activation, return_cls=True)
-            self._out_act = out_act_cls(name=prefix+out_activation if out_activation else '')
+            self._flat = layers.Flatten(name=prefix+'flatten')
             self.out_size = out_size
             if deter_stoch:
                 if out_size is None:
                     f = filters[-1]
                     subsample_kwargs['filters'] = f
                     ds_cls = block_registry.get('dsl')
-                    self._layers += [
-                        ds_cls(n_blocks=n_blocks[-1],
-                            subsample=subsample, 
-                            subsample_kwargs=subsample_kwargs, 
-                            block=block,
-                            block_kwargs=block_kwargs,
-                            name=f'{prefix}ds')
-                    ]
+                    self._ds_layer = ds_cls(n_blocks=n_blocks[-1],
+                        subsample=subsample, 
+                        subsample_kwargs=subsample_kwargs, 
+                        block=block,
+                        block_kwargs=block_kwargs,
+                        name=f'{prefix}ds')
+                    self._layers += [self._ds_layer]
                     self._training_cls.append(ds_cls)
-                    self._flat = layers.Flatten(name=prefix+'flatten')
                 else:
-                    self._flat = layers.Flatten(name=prefix+'flatten')
                     ds_cls = block_registry.get('dss')
-                    
-                    self._layers += [
-                        ds_cls('dense',
-                            self.out_size, 
-                            activation=self._out_act, 
-                            name=prefix+'out')
-                    ]
+                    # NOTE: try adding a relu here 
+                    self._dense = self._ds_layer = ds_cls(
+                        'dense',
+                        self.out_size, 
+                        activation=out_act_cls(), 
+                        name=prefix+'out')
             else:
-                self._flat = layers.Flatten(name=prefix+'flatten')
                 if self.out_size:
-                    self._dense = layers.Dense(self.out_size, activation=self._out_act, name=prefix+'out')
+                    self._dense = layers.Dense(self.out_size, activation=out_act_cls(), name=prefix+'out')
+            self._layers += [out_act_cls(name=prefix+out_activation)]
+
 
         self._training_cls += [block_cls, subsample_cls, sa_cls]
     
@@ -123,13 +120,12 @@ class ProcgenCNN(Module):
             t = x.shape[1]
             x = tf.reshape(x, [-1, *x.shape[2:]])
         x = super().call(x, training=training)
-        if self._deter_stoch:
-            self.state = self._layers[-1].state
-        x = self._out_act(x)
         self.cnn_out = x
         x = self._flat(x)
         if self._time_distributed:
             x = tf.reshape(x, [-1, t, *x.shape[1:]])
         if self.out_size:
             x = self._dense(x)
+        if self._deter_stoch:
+            self.state = self._ds_layer.state
         return x