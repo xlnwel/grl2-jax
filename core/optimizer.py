64a53009a5c9006a359b7eae75391f1d0ca552b9
the_raven_chaser <122134545@qq.com>
2021-01-11 09:01:30 +0800

    Add model-wise gradient scales

--- core/optimizer.py
+++ core/optimizer.py
@@ -1,7 +1,7 @@
 import re
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras.mixed_precision import experimental as prec
+
 
 def select_optimizer(name):
     opts = dict(
@@ -12,11 +12,16 @@ def select_optimizer(name):
 
 
 class Optimizer(tf.Module):
-    def __init__(self, name, models, lr, clip_norm=None, weight_decay=None, wdpattern=r'.*', return_grads=False, **kwargs):
+    def __init__(self, name, models, lr, clip_norm=None, weight_decay=None, 
+                wdpattern=r'.*', scales=None, return_grads=False, **kwargs):
         self._models = models if isinstance(models, (list, tuple)) else [models]
         self._clip_norm = clip_norm
         self._weight_decay = weight_decay
         self._wdpattern = wdpattern
+        if scales is not None:
+            assert isinstance(scales, (list, tuple)), scales
+            assert len(scales) == len(self._models), (len(scales), len(self._models))
+        self._scales = scales
         self._opt = select_optimizer(name)(lr, **kwargs)
         self._return_grads = return_grads
         # useful for mixed precision training on GPUs to
@@ -36,6 +41,10 @@ class Optimizer(tf.Module):
         if self._variables is None:
             variables = [m.trainable_variables for m in self._models]
             self._variables = tf.nest.flatten(variables)
+            if self._scales is not None:
+                scales = [[self._scales[i] for _ in m.trainable_variables] 
+                    for i, m in enumerate(self._models)]
+                self._scales = tf.nest.flatten(scales)
         assert loss.shape == (), loss.shape
         if self._mpt:
             with tape:
@@ -44,6 +53,9 @@ class Optimizer(tf.Module):
         assert None not in grads, f'No grads for {self._variables[grads.index(None)].name}'
         if self._mpt:
             grads = self._opt.get_unscaled_gradients(grads)
+        if self._scales is not None:
+            assert len(grads) == len(self._scales), (len(grads), len(self._scales))
+            grads = [g * s for g, s in zip(grads, self._scales)]
         norm = tf.linalg.global_norm(grads)
         if self._clip_norm:
             grads, _ = tf.clip_by_global_norm(grads, self._clip_norm, norm)
