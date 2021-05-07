82284a955008b1beda763c040ae70fd508f5aa4a
the_raven_chaser <122134545@qq.com>
2021-05-07 22:41:47 +0800

    Add choice for huber loss

--- algo/ppo/base.py
+++ algo/ppo/base.py
@@ -2,7 +2,7 @@ import logging
 import tensorflow as tf
 
 from utility.tf_utils import explained_variance
-from utility.rl_loss import reduce_mean, ppo_loss, ppo_value_loss
+from utility.rl_loss import huber_loss, reduce_mean, ppo_loss, ppo_value_loss
 from utility.schedule import TFPiecewiseSchedule
 from core.base import RMSAgentBase
 from core.decorator import override, step_track
@@ -19,6 +19,8 @@ class PPOBase(RMSAgentBase):
     @override(RMSAgentBase)
     def _add_attributes(self, env, dataset):
         super()._add_attributes(env, dataset)
+
+        self._huber_threshold = getattr(self, '_huber_threshold', None)
 
         self._last_obs = None   # we record last obs before training to compute the last value
         self._value_update = getattr(self, '_value_update', None)
@@ -72,7 +74,8 @@ class PPOBase(RMSAgentBase):
                 log_ratio, advantage, self._clip_range, entropy)
             # value loss
             value = self.value(x)
-            value_loss, v_clip_frac = self._compute_value_loss(value, traj_ret, old_value)
+            value_loss, v_clip_frac = self._compute_value_loss(
+                value, traj_ret, old_value)
             
             actor_loss = (policy_loss - self._entropy_coef * entropy)
             value_loss = self._value_coef * value_loss
@@ -97,11 +100,19 @@ class PPOBase(RMSAgentBase):
     def _compute_value_loss(self, value, traj_ret, old_value, mask=None):
         value_loss_type = getattr(self, '_value_loss', 'mse')
         v_clip_frac = 0
+        if value_loss_type == 'huber':
+            value_loss = reduce_mean(
+                huber_loss(value, traj_ret, threshold=self._huber_threshold), mask)
         if value_loss_type == 'mse':
             value_loss = .5 * reduce_mean((value - traj_ret)**2, mask)
         elif value_loss_type == 'clip':
             value_loss, v_clip_frac = ppo_value_loss(
-                value, traj_ret, old_value, self._clip_range, mask)
+                value, traj_ret, old_value, self._clip_range, 
+                mask=mask)
+        elif value_loss_type == 'clip_huber':
+            value_loss, v_clip_frac = ppo_value_loss(
+                value, traj_ret, old_value, self._clip_range, 
+                mask=mask, threshold=self._huber_threshold)
         else:
             raise ValueError(f'Unknown value loss type: {value_loss_type}')
         return value_loss, v_clip_frac
