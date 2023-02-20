396e3943147b4ee9e97faf374a8850a850ee5f04
the_raven_chaser <122134545@qq.com>
2023-02-20 17:56:25 +0800

    Call it a day.

--- algo/mambpo/elements/actor.py
+++ algo/mambpo/elements/actor.py
@@ -1,13 +1,1 @@
-from core.elements.actor import Actor as ActorBase, create_actor
-
-class Actor(ActorBase):
-    def __call__(self, inp, evaluation):
-        out = self.model.action(inp, evaluation)
-        out = self._process_output(inp, out, evaluation)
-        return out
-
-    def _process_output(self, inp, out, evaluation):
-        return out
-
-def create_actor(config, model, name='actor'):
-    return Actor(config=config, model=model, name=name)
+from algo.masac.elements.actor import *
--- algo/mambpo/elements/buffer.py
+++ algo/mambpo/elements/buffer.py
@@ -1,10 +1,1 @@
-from replay.uniform import UniformReplay
-
-
-def create_buffer(config, model, env_stats, **kwargs):
-    return UniformReplay(
-        config=config, 
-        env_stats=env_stats, 
-        model=model, 
-        **kwargs
-    )
+from algo.masac.elements.buffer import *
