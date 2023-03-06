Test the idea of MASAC+LKA. Fix model obs normalization.

algo/dynamics/elements/trainer.py
@@ -44,9 +44,6 @@ class Trainer(TrainerBase):
 
     def build_optimizers(self):
         theta = self.model.theta.copy()
-        if self.config.obs_normalization:
-            theta.pop("obs_normalizer_params")
-            theta.pop("diff_normalizer_params")
         self.opts.theta, self.params.theta = optimizer.build_optimizer(
             params=theta, 
             **self.config.model_opt, 
@@ -67,10 +64,6 @@ class Trainer(TrainerBase):
         elite_indices = np.argsort(stats.mean_loss)
         self.model.rank_elites(elite_indices)
         self._evaluate_model(stats)
-
-        # update normalization parameters
-        if self.config.obs_normalization:
-            self.model.update_normalizers(data.obs, data.next_obs)
 
         data = flatten_dict({f'data/{k}': v 
             for k, v in data.items() if v is not None})
@@ -106,13 +99,6 @@ class Trainer(TrainerBase):
         data, 
     ):
         do_logging('train is traced', backtrack=4)
-        if self.config.obs_normalization:
-            extra_kwargs = {
-                'obs_normalizer_params': theta.pop('obs_normalizer_params'),
-                'diff_normalizer_params': theta.pop('diff_normalizer_params'),
-            }
-        else:
-            extra_kwargs = {}
         theta, opt_state, stats = optimizer.optimize(
             self.loss.loss, 
             theta, 
@@ -120,7 +106,6 @@ class Trainer(TrainerBase):
             kwargs={
                 'rng': rng, 
                 'data': data, 
-                **extra_kwargs
             }, 
             opt=self.opts.theta, 
             name='train/dynamics'
@@ -131,6 +116,14 @@ class Trainer(TrainerBase):
     def process_data(self, data):
         if self.env_stats.is_action_discrete[0]:
             data.action = self.model.process_action(data.action)
+        
+        if self.model.config.model_norm_obs:
+            data, rms = data
+            if rms is not None:
+                self.model.obs_rms.update_from_moments(*rms)
+            data.obs = self.model.obs_rms.normalize(data.obs)
+            data.next_obs = self.model.obs_rms.normalize(data.next_obs)
+
         return data
 
     def _evaluate_model(self, stats):

replay/uniform.py
@@ -7,6 +7,7 @@ from core.elements.model import Model
 from core.typing import AttrDict, tree_slice, subdict
 from replay.local import NStepBuffer
 from tools.utils import batch_dicts
+from tools.rms import RunningMeanStd
 from replay import replay_registry
 
 
@@ -31,6 +32,8 @@ class UniformReplay(Buffer):
 
         self._memory = collections.deque(maxlen=self.max_size)
 
+        if self.config.model_norm_obs:
+            self.obs_rms = RunningMeanStd([0])
         self._tmp_bufs: List[NStepBuffer] = [
             NStepBuffer(config, env_stats, model, aid, 0) 
             for _ in range(self.n_envs)
@@ -90,6 +93,9 @@ class UniformReplay(Buffer):
         if isinstance(trajs, dict):
             trajs = [trajs]
         self._memory.extend(trajs)
+        if self.config.model_norm_obs:
+            for traj in trajs:
+                self.obs_rms.update(traj['obs'])
 
     def merge_and_pop(self, trajs):
         if isinstance(trajs, dict):
@@ -159,6 +165,14 @@ class UniformReplay(Buffer):
 
     def _get_samples(self, idxes, memory):
         fn = lambda x: np.expand_dims(np.stack(x), 1)
-        results = batch_dicts([memory[i] for i in idxes], func=fn)
+        samples = batch_dicts([memory[i] for i in idxes], func=fn)
 
-        return results
+        if self.config.model_norm_obs:
+            if self.obs_rms.is_initialized():
+                samples = samples, self.obs_rms.get_rms_stats()
+            else:
+                samples = samples, None
+            self.obs_rms.reset_rms_stats()
+            assert not self.obs_rms.is_initialized(), (self.obs_rms._count, self.obs_rms._epsilon, self.obs_rms.is_initialized())
+
+        return samples